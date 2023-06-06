fb8ff7695ea06dde9709fb4a082dc93ecc16d0d5
NaserKh <naserK@360medlink.com>
2023-06-06 17:44:45 +0100

    [gatway-ms]Refactored RouteServiceProvider: configured rate limiting, mapped external service routes, and defined API and web routes.

--- gatway-ms/app/Providers/RouteServiceProvider.php
+++ gatway-ms/app/Providers/RouteServiceProvider.php
@@ -2,39 +2,124 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to your application's "home" route.
+     * The path to the "home" route for your application.
      *
-     * Typically, users are redirected here after authentication.
+     * This is used by Laravel authentication to redirect users after login.
      *
      * @var string
      */
     public const HOME = '/home';
 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * The base module namespace.
+     *
+     * @var string
      */
-    public function boot(): void
+    protected const BASE_INTERNAL_MODULE_NAMESPACE = 'App\ExternalServices';
+
+    /**
+     * The base module path.
+     *
+     * @var string
+     */
+    protected const BASE_MODULE_PATH = 'app/ExternalServices';
+
+    /**
+     * The names of external services.
+     *
+     * @var array
+     */
+    protected $externalServiceNames = [
+        'Authorization',
+        'TaskManagement',
+    ];
+
+
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        $this->configureRateLimiting();
+        $this->mapServicesRoutes();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+    }
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+
+    /**
+     * Map the routes for external services.
+     *
+     * @return void
+     */
+    protected function mapServicesRoutes()
+    {
+        foreach ($this->externalServiceNames as $service) {
+
+            $routeFile = self::BASE_MODULE_PATH . '/' . $service . '/Routes/api.php';
+
+            $this->defineApiRoutes(self::BASE_INTERNAL_MODULE_NAMESPACE . '\\' . $service . "\Controllers", $routeFile);
+        }
+    }
+
+    /**
+     * Define API routes.
+     *
+     * @param  string  $namespace
+     * @param  string  $routeFile
+     * @return void
+     */
+    protected function defineApiRoutes($namespace, $routeFile)
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($namespace)
+            ->group(base_path($routeFile));
+    }
+
+    /**
+     * Define web routes.
+     *
+     * @param  string  $namespace
+     * @param  string  $routeFile
+     * @return void
+     */
+    protected function defineWebRoutes($namespace, $routeFile)
+    {
+        Route::middleware('web')
+            ->namespace($namespace)
+            ->group(base_path($routeFile));
     }
 }
