b1d0dfab71f5c5393ed50c7a7aba3917408d32c0
NaserKh <naserK@360medlink.com>
2023-06-09 12:51:37 +0100

    [gatway-ms]authorization : Define the login() method with the  parameter

--- gatway-ms/app/ExternalServices/Authorization/Contracts/AuthorizationServiceInterface.php
+++ gatway-ms/app/ExternalServices/Authorization/Contracts/AuthorizationServiceInterface.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Contracts;
+namespace App\ExternalServices\Authorization\Contracts;
 
 use App\ExternalServices\Contracts\BaseApiServiceInterface;
 
 interface AuthorizationServiceInterface   extends BaseApiServiceInterface
 {
+    public function login($request_data);
 }
