c4a50411d2cbc5137e69425ee4641d711ca1993f
NaserKh <naserK@360medlink.com>
2023-06-09 16:13:47 +0100

    [authorization-ms]add middleware to check secret key from request  allowed

---
+++ authorization-ms/app/Http/Middleware/AuthenticateAccessMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateAccessMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+     
+        // explode then check for the token if  token exists proceed or else abort
+        $validSecrets = explode(',',config('services.secret') );
+     
+        if (in_array($request->header('SecretKey'), $validSecrets)) {
+
+            return $next($request);
+        }
+
+          return response()->json(['message' => 'Externel Request Unauthorized Service !  '], Response::HTTP_UNAUTHORIZED);
+    }
+}
