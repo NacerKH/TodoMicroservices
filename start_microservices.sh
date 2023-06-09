c42689fc648be00690cd471d0c4c2094bf4d1cb3
NaserKh <naserK@360medlink.com>
2023-06-09 20:22:13 +0100

    [setup]Updated script to prompt the user for their preference

--- start_microservices.sh
+++ start_microservices.sh
@@ -7,15 +7,45 @@ directories=(
     "./taskManagement-ms"
     # Add more directories as needed
 )
+echo "==========================="
+echo "Starting microservices"
+echo "Your Computer is having a hard time running all the microservices at once. Dont worry!"
+echo "I have a solution for you.   🌟 " 
+echo "i make local-compose.yml files for you. " 
+echo " One Sql service for all microservices. each microservice has database with different name.  🎉   "
+echo " Use Nginx web server Part of image PHP not separatly service webserver.  🚀 "
 
-# Iterate over each directory and start microservices
-for dir in "${directories[@]}"
-do
-    echo "Starting microservices in directory: $dir"
-    cd "$dir"
-    pwd
-    docker-compose -f local-compose.yml up -d --build  --remove-orphans 
-        echo "==========================="
-        echo "Docker-compose exited with status $?" 
-    cd ..
-done
+echo  "Do you want local-compose less Ressource ? [Y/n] " 
+
+read REPLY 
+echo $REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "==========================="
+    echo "Starting microservices with local-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Starting microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose -f local-compose.yml up -d --build  --remove-orphans 
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+else
+    echo "==========================="
+    echo "Starting microservices with docker-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Starting microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose up -d --build  --remove-orphans 
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+fi
+
+echo "==========================="
--- stop_microservices.sh
+++ stop_microservices.sh
@@ -7,15 +7,36 @@ directories=(
     "./taskManagement-ms"
     # Add more directories as needed
 )
+echo  "What You use local-compose ? [Y/n] 🌀 "
+read REPLY 
+echo $REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "==========================="
+    echo "Stoping microservices with local-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Stoping microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose -f local-compose.yml down
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+else
+    echo "==========================="
+    echo "Stoping microservices with docker-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Stoping microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose down
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+fi
 
-# Iterate over each directory and stop microservices
-for dir in "${directories[@]}"
-do
-    echo "stop docker-compose in directory: $dir"
-    cd "$dir"
-    pwd
-    docker-compose -f local-compose.yml down
-        echo "==========================="
-        echo "Docker-compose exited with status $?" 
-    cd ..
-done
+echo "==========================="
