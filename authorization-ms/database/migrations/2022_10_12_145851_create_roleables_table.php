e7c6d22f815608166cac82ee4149fdd43c6bb00c
NaserKh <naserK@360medlink.com>
2023-06-09 16:15:44 +0100

    [authorization-ms]make schema authorization principe

---
+++ authorization-ms/database/migrations/2022_10_12_145851_create_roleables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roleables', function (Blueprint $table) {
+            $table->id();
+            $table->integer("role_id");
+            $table->integer("roleable_id");
+            $table->string("roleable_type");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roleables');
+    }
+};
