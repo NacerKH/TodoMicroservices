7a720652d10f56fffbce5601c2bdcc9f8e599188
NaserKh <naserK@360medlink.com>
2023-06-09 17:58:43 +0100

    [authorization-ms][Athentification]make Form Request validation for sign In & Up

---
+++ authorization-ms/app/Modules/Authentication/Requests/RegistrationRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Modules\Authentication\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegistrationRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|unique:users,email',
+            'password' => [
+                'required',
+                'string',
+                'confirmed',
+                'min:6',
+                'regex:/[a-z]/',
+                'regex:/[A-Z]/',
+                'regex:/[0-9]/',
+                'regex:/[@$!%*#?&]/',
+            ],
+  
+        ];
+    }
+}
