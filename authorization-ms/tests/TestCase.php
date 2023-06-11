6a7d84aeaee779d07a7de2c5e72d8f2a736657e9
NaserKh <naserK@360medlink.com>
2023-06-11 01:30:50 +0100

    [backend][auth-ms]unit_test: seeding data

--- authorization-ms/tests/TestCase.php
+++ authorization-ms/tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication ;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+
+     
+    }
 }
