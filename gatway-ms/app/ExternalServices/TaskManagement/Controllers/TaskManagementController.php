908d283b76913294e3a340046496a8fd6121f55e
NaserKh <naserK@360medlink.com>
2023-06-12 23:37:25 +0100

    [backend][gatway-ms]feat_taskManagemnt: cntrl Task crud proxy

--- gatway-ms/app/ExternalServices/TaskManagement/Controllers/TaskManagementController.php
+++ gatway-ms/app/ExternalServices/TaskManagement/Controllers/TaskManagementController.php
@@ -3,28 +3,47 @@ namespace App\ExternalServices\TaskManagement\Controllers;
 
 
 
-use App\ExternalServices\Authorization\Services\AuthorizationService;
+
+use App\ExternalServices\TaskManagement\Services\TaskManagementService;
 use App\Http\Controllers\Controller;
 use App\Traits\BaseApiResponse;
+use Illuminate\Http\Request;
 
-
-class AuthorizationController extends Controller
+class TaskManagementController extends Controller
 {
     use BaseApiResponse;
-    /**
-     * @var AuthorizationService
-     */
 
 
     /**
-     * AuthorizationController constructor.
-     * @param AuthorizationService $authorizationService
+     * TaskManagementController constructor.
+     * @param TaskManagementService $taskManagementService
      * 
      */
-    public function __construct(private AuthorizationService $authorizationService)
+    public function __construct(private TaskManagementService $taskManagementService){}
+    
+
+    public function index()
     {
-   
+        return $this->taskManagementService->index();
+    }
+    public function store(Request $request)
+    {
+        return $this->taskManagementService->store($request->all());
     }
 
+    public function show($resource_id)
+    {
+        return $this->taskManagementService->show($resource_id);
+    }
+ 
 
+    public function update(Request $request, $resource_id)
+    {
+        return $this->taskManagementService->update($resource_id, $request->all());
+    }
+
+    public function destroy($resource_id)
+    {
+        return $this->taskManagementService->delete($resource_id);
+    }
 }
