7dda9d70cec09e76e27114db0312ec7325fcb706
NaserKh <naserK@360medlink.com>
2023-06-12 01:04:46 +0100

    [backend][taskmangemnt-ms]feat:trait responser & base interface crud and base repo

---
+++ taskManagement-ms/app/Modules/Concerns/BaseApiRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Modules\Concerns;
+
+use App\Models\User;
+
+
+class BaseApiRepository
+{
+    // Traits
+    use BaseApiResponse;
+
+
+    // /**
+    //  * Logs the login activity for the specified user.
+    //  *
+    //  * @param User $user The user object.
+    //  * @return void
+    //  */
+    // public function loginLog($user , $class_name  ,$action) 
+    // {
+
+    //     ActivityLog::create([
+    //         'user_id' => $user->id,
+    //         'user_role' =>  $user->role,
+    //         'class_name' => $class_name,
+    //         'action' => $action,
+    //         'title' => $user->name  . ' in  ' . $class_name . ' process ' . $action,
+    //         'data' => '',
+    //     ]);
+    // }
+
+}
---
+++ taskManagement-ms/app/Modules/Concerns/BaseApiRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Modules\Concerns;
+
+
+interface BaseApiRepositoryInterface
+{
+    public function index();
+
+    public function store($form_data);
+
+    public function show($resource_id);
+
+    public function update($resource_id, $form_data);
+
+    public function delete($resource_id);
+}
