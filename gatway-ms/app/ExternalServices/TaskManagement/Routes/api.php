c80c77ea9ab05db473ba754337b3f05957b589f7
NaserKh <naserK@360medlink.com>
2023-06-12 01:07:39 +0100

    [backend][gatway-ms]feat: ping from gatway

--- gatway-ms/app/ExternalServices/TaskManagement/Routes/api.php
+++ gatway-ms/app/ExternalServices/TaskManagement/Routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
-
+use App\ExternalServices\TaskManagement\Controllers\TaskManagementController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('TaskManagement')->group(function () {
     Route::get('/test', function () {
         return 'TaskManagement';
     });
-
+   Route::apiResource('list-task', TaskManagementController::class)->middleware('checkAuth');
 });
 
 
