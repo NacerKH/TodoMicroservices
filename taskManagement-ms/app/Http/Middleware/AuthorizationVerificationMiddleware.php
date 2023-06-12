488572b2d74e8a260cc2d6a623ce5ee99d3a4b25
NaserKh <naserK@360medlink.com>
2023-06-12 23:38:16 +0100

    [backend][taskmangemnt-ms]feat:checking User_id header Middleware

---
+++ taskManagement-ms/app/Http/Middleware/AuthorizationVerificationMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizationVerificationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userId = $request->header('X-User-Id');
+
+        if (!$userId) {
+            // Handle the case where X-User-Id header is missing
+            return response()->json(['error' => 'Unauthorized ! TaskManagemt-ms'], Response::HTTP_UNAUTHORIZED);
+        }
+        return $next($request);
+    }
+}
