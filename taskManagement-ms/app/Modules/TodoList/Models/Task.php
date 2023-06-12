cb193abea7dfb5b694250a7460f847060718184f
NaserKh <naserK@360medlink.com>
2023-06-13 00:54:57 +0100

    [backend][taskmangemnt-ms]feat_model:append status attr & map factory

--- taskManagement-ms/app/Modules/TodoList/Models/Task.php
+++ taskManagement-ms/app/Modules/TodoList/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\TodoList\Models;
 
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,8 +21,8 @@ class Task extends Model
         'content',
         'priority',
         'date_of_completion',
-        'status',
-        'list_task_id'
+        'list_task_id',
+        'user_id'
     ];
     /**
      * The attributes that should be cast.
@@ -32,9 +33,22 @@ class Task extends Model
         'date_of_completion' => 'datetime',
         'updated_at' => 'datetime:Y-m-d H:00',
     ];
+   
+    protected $appends =['status'];
+    protected static function newFactory()
+    {
+        return TaskFactory::new();
+    }  
+    
     public function listTask()
     {
 
         return $this->belongsTo(ListTask::class, 'list_task_id', 'id');
+    }
+
+    public function getStatusAttribute()
+    {
+
+        return $this->listTask()->first()?->name;
     }
 }
