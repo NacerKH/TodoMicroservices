c3587554932e6b0d567e74ab1c33949045c6867a
NaserKh <naserK@360medlink.com>
2023-06-13 00:53:51 +0100

    [backend][taskmangemnt-ms]feat_routing: apiRessource Task and list fix

--- taskManagement-ms/app/Modules/TodoList/Routes/api.php
+++ taskManagement-ms/app/Modules/TodoList/Routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Modules\TodoList\Controllers\ListTaskController;
+use App\Modules\TodoList\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('TaskManagement')->group(function () {
 
-Route::apiResource('/', ListTaskController::class);
+Route::apiResource('/list-task', ListTaskController::class)->parameters(['' => 'listTask']);;
+Route::apiResource('/task', TaskController::class)->parameters(['' => 'task']);;
 });
