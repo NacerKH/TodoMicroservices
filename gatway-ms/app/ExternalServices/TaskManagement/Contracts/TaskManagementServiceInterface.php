f485384f0c80f8af7087d16c13bd3484a91dd77d
NaserKh <naserK@360medlink.com>
2023-06-13 17:38:57 +0100

    [backend][gatway-ms]add fn signiature

--- gatway-ms/app/ExternalServices/TaskManagement/Contracts/TaskManagementServiceInterface.php
+++ gatway-ms/app/ExternalServices/TaskManagement/Contracts/TaskManagementServiceInterface.php
@@ -7,4 +7,6 @@ interface TaskManagementServiceInterface   extends BaseApiServiceInterface
 {
     public function UserGetHisTask();
     public function UserAssignTask($task_id, $request_data);
+    public function UserChangeStatusTask($task_id, $request_data);
+    public function UserChangePriorityTask($task_id, $request_data);
 }
