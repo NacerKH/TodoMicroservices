720c8d0c450332ebf5655106ed90055c74786b72
NaserKh <naserK@360medlink.com>
2023-06-13 17:14:40 +0100

    [backend][taskmangemnt-ms]feat: add new fn signture chng status & priority

--- taskManagement-ms/app/Modules/TodoList/Contracts/TaskRepositoryInterface.php
+++ taskManagement-ms/app/Modules/TodoList/Contracts/TaskRepositoryInterface.php
@@ -8,6 +8,9 @@ interface TaskRepositoryInterface extends BaseApiRepositoryInterface
 
   public function  findTaskByUser($user_id);
   public function  assigneTask($resource_id, $user_id);
+  public function ChangePriority($task, $priority);
+  public function  ChangeStatus($task, $status);
+  
 
  
 }
