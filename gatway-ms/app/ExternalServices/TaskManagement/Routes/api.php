ac6b3a6f93428b8dd01597b688b9d4b6acacbf8d
NaserKh <naserK@360medlink.com>
2023-06-13 17:39:21 +0100

    [backend][gatway-ms]add fn chng status & priorty routes

--- gatway-ms/app/ExternalServices/TaskManagement/Routes/api.php
+++ gatway-ms/app/ExternalServices/TaskManagement/Routes/api.php
@@ -14,7 +14,9 @@ Route::prefix('TaskManagement')->middleware(['checkAuth'])->group(function () {
 
    Route::post('task/assign/{id}', [TaskManagementController::class, 'assign']);
 
-   Route::get('task/User/Tasks', [TaskManagementController::class, 'userTasks']);
+   Route::get('task/user/Tasks', [TaskManagementController::class, 'userTasks']);
+    Route::post('task/user/change-priority/{id}', [TaskManagementController::class, 'changePriority']);
+    Route::post('task/user/change-status/{id}', [TaskManagementController::class, 'changeStatus']);
 });
 
 
