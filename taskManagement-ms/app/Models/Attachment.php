32817ee51f07c898e13c13ff2e47aaf22507bba2
NaserKh <naserK@360medlink.com>
2023-06-15 12:12:14 +0100

    [backend][taskmangemnt-ms] add attachemnt model polymorphic relationshp

---
+++ taskManagement-ms/app/Models/Attachment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'filename',
+        'path'
+    ];
+    public function attachable()
+    {
+        return $this->morphTo();
+    }
+
+
+}
