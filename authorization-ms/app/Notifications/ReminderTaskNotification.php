18317c04bc4df85cad3ccb33e61f5db8ed8314ee
NaserKh <naserK@360medlink.com>
2023-06-15 03:38:32 +0100

    [backend][authrization-ms] make Reminder notification should queue

---
+++ authorization-ms/app/Notifications/ReminderTaskNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReminderTaskNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+   
+    public $task;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)->subject('Task Reminder')
+            ->line('The task will be completed on ' . $this->task['date_of_completion'])
+            ->action('Task Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
