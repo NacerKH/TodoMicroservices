e991937adae45fc227bb68ed06f3719e2bd9fb7e
NaserKh <naserK@360medlink.com>
2023-06-18 01:03:47 +0100

    [backend][taskManagment-ms] refactory add arg to test event

--- taskManagement-ms/app/Console/Commands/ReminderTaskEventsCommand.php
+++ taskManagement-ms/app/Console/Commands/ReminderTaskEventsCommand.php
@@ -6,7 +6,7 @@ namespace App\Console\Commands;
 use App\Modules\TodoList\Models\Task;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
-
+use Illuminate\Support\Str;
 class ReminderTaskEventsCommand extends Command
 {
     /**
@@ -14,29 +14,41 @@ class ReminderTaskEventsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'task:events';
+    protected $signature = 'task:events {test?}';
 
     /**
      * The console command description.com
      *
      * @var string
      */
-    protected $description = 'task will remind you of the tasks that are due in the next 24 hours';
+    protected $description = 'This command reminds you of tasks that are due within the next 24 hours.';
+
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $tasks = Task::where('date_of_completion', '<=', now()->addDay())
-            ->where('date_of_completion', '>=', now())
-            ->get();
-    print_r($tasks);
- 
-          Redis::publish('reminder-task', json_encode(
-            ['event'=> 'reminder-task',
-                    'data' =>$tasks]
-                ));
-        
+      
+     if ($this->argument('test') == "test" ){
+      $tasks = Task::get();
+     }
+      else {
+      $tasks = Task::where('date_of_completion', '<=', now()->addDay())
+        ->where('date_of_completion', '>=', now())
+        ->get();
+      }
+    
+        print_r($tasks);
+      // Set prefix configuration
+        \Config::set('database.redis.options.prefix', 'todo_authorization_database_');
+
+        Redis::publish('reminder-task', json_encode(
+        ['event'=> 'reminder-task',
+                'data' =>$tasks]
+            ));
+
+      // Reset configuration
+        \Config::set('database.redis.options.prefix', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_');
     }
 }
