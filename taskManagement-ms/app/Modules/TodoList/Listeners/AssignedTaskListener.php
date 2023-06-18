e951187e41cee39cb0af7b9592e766d122e67790
NaserKh <naserK@360medlink.com>
2023-06-18 16:44:08 +0100

    [task-management-ms][redis] assigned listen config brodcast prefix

--- taskManagement-ms/app/Modules/TodoList/Listeners/AssignedTaskListener.php
+++ taskManagement-ms/app/Modules/TodoList/Listeners/AssignedTaskListener.php
@@ -6,6 +6,7 @@ use App\Modules\TodoList\Events\AssignedTaskEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Str;
 
 class AssignedTaskListener
 {
@@ -22,11 +23,19 @@ class AssignedTaskListener
      */
     public function handle(AssignedTaskEvent $event): void
     {
+            // Set the Redis prefix to include the authorization microservice name
+         \Config::set('database.redis.options.prefix', Str::slug(env('APP_NAME_AUTHORIZATION_MS', 'laravel'), '_') . '_database_');
+
+         // Publish the task-was-assigned event to Redis
         Redis::publish('task-was-assigned', json_encode(
             [
                 'event' => 'task-was-assigned',
                 'data' => $event
             ]
         ));
+        // Reset the Redis prefix to the default value
+        \Config::set('database.redis.options.prefix', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_');
+
+
     }
 }
